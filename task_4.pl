e0e5f45d34d0abfb14010bb85e6096967e1313ef
Roman Batin <roman.batin@qoollo.com>
2020-10-19 22:56:05 +0300

    refactor task_4

--- task_4.pl
+++ task_4.pl
@@ -1,16 +1,15 @@
-pack_sim_elems([], _, ElemAcc, ListAcc,Res) :- 
+pack_sim_elems([], ElemAcc, ListAcc,Res) :- 
     append(ListAcc, [ElemAcc], Res), !.
 
-pack_sim_elems([H|L], H, ElemAcc, ListAcc, Res) :-
-    pack_sim_elems(L, H, [H | ElemAcc], ListAcc, Res), !.
+pack_sim_elems([AccumElem|L], [AccumElem|Elems], ListAcc, Res) :-
+    append([AccumElem|Elems], [AccumElem], Accum),
+    pack_sim_elems(L, Accum, ListAcc, Res), !.
 
-pack_sim_elems([H1|L], H2, ElemAcc, ListAcc,Res) :- 
-    not(H1 = H2),
+pack_sim_elems([NewElem|L], ElemAcc, ListAcc,Res) :- 
     append(ListAcc, [ElemAcc], List), 
-    pack_sim_elems([H1|L], H1, [], List, Res), !.
+    pack_sim_elems(L, [NewElem], List, Res).
 
 pack([], Res) :- Res = [], !.
 pack([H | L], Res) :-
-    pack_sim_elems(L, H, [H], [], Res).
-
+    pack_sim_elems(L, [H], [], Res).
 % Task 4 pack([a,c, b,c,c,c, b, 1, 1, a, a, a, d, b], X)
